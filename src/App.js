7c8e15e9d6401d9f2187669c239ead6dceb82d27
George Ijidola <geordola@gmail.com>
2019-08-18 01:31:41 +0100

    Version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+// States
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
+// Utilities
+import Navbar from './components/layouts/Navbar'
+import Alert from './components/layouts/Alert'
+
+// Small Components
+import User from './components/users/User'
+
+// Page Components
+import About from './components/pages/About'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
+
+// Stylesheets
+import './App.css'
+import './fonts/css/all.css'
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  )
+}
+
+export default App
